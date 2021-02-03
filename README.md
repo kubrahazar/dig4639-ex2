41a4d2f207cd83a8ea9764a313969fa9751b74ed
kubrahazar <knight@ucf.edu>
2021-02-03 09:22:47 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dig4639-ex2
+
+"The way to get started is to quit talking and begin doing."
+    -Walt Disney
---
+++ index.js
@@ -0,0 +1,12 @@
+let quotes = [
+    `"The way to get started is to quit talking and begin doing."
+    -Walt Disney`,
+    `"Whoever is happy will make others happy too."
+    -Anne Frank`,
+    `"If life were predictable it would cease to be life, and be without flavor."
+    -Eleanor Roosevelt`
+]
+
+let index = console.log(Math.floor(Math.random() * 3))
+
+console.log(quotes[index])
